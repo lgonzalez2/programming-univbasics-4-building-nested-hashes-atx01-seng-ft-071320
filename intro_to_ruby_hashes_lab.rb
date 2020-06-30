c522a87dd9030baa69c2ebd04bd7ee328594e0e5
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-06-30 23:32:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -46,7 +46,9 @@ ex_hash = {
         :four_pieces_owned => 200
       }, 
       :names => {
-        :reading_railroad => {},
+        :reading_railroad => {
+          :mortgage_value => 100
+        },
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
         :shortline_railroad => {}

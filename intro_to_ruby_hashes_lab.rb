d6d9ad9b1966d24d4edf03e2eb7ac9895a8b84da
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-06-30 23:26:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,7 +6,13 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  
+  ex_hash = {
+    :railroads => {
+      :pieces => 4, 
+      :rent_in_dollars => {}, 
+      :names => {}
+    }
+  }
 end
 
 def monopoly_with_third_tier
